cd7c8c14d08080473109f815c1ea970d104b2e4c
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-02-28 11:50:24 -0800

    use mem_release instead when dropping

--- lib/runtime-core/src/sys/windows/memory.rs
+++ lib/runtime-core/src/sys/windows/memory.rs
@@ -8,6 +8,7 @@ use winapi::um::winnt::{
     MEM_COMMIT, MEM_DECOMMIT, MEM_RESERVE, PAGE_EXECUTE_READ, PAGE_NOACCESS, PAGE_READONLY,
     PAGE_READWRITE,
 };
+use winapi::um::winnt::MEM_RELEASE;
 
 unsafe impl Send for Memory {}
 unsafe impl Sync for Memory {}
@@ -165,7 +166,7 @@ impl Memory {
 impl Drop for Memory {
     fn drop(&mut self) {
         if !self.ptr.is_null() {
-            let success = unsafe { VirtualFree(self.ptr as _, self.size, MEM_DECOMMIT) };
+            let success = unsafe { VirtualFree(self.ptr as _, self.size, MEM_RELEASE) };
             // If the function succeeds, the return value is nonzero.
             assert_eq!(success, 1, "failed to unmap memory: {}", errno::errno());
         }
